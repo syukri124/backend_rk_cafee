540f48bcf86f215e05bd328e26a5efcb3192ff5e
syukri124 <syukriihsan680@gmail.com>
2025-12-11 12:34:02 +0700

    deploy

--- config/database.js
+++ config/database.js
@@ -1,22 +1,24 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false, // Matikan log SQL di console agar bersih
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+    acquire: 30000
   }
-);
+});
 
 module.exports = sequelize;
